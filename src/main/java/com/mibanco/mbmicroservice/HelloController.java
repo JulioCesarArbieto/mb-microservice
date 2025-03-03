37ff03395f93532b23624ba986e5f26fcc62d2d1
JulioArbieto <arbieto.jc@gmail.com>
2025-03-03 10:36:14 -0500

    Add command kubectl

--- src/main/java/com/mibanco/mbmicroservice/HelloController.java
+++ src/main/java/com/mibanco/mbmicroservice/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/saludo") // Define la ruta base opcionalmente
+@RequestMapping("/")
 public class HelloController {
     @GetMapping
     public String hello() {
